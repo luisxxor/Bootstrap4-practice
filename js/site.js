fc802506190f34544ff889b847972eb4e47ed6f7
luisxxor <luisxxor@gmail.com>
2018-01-08 23:10:58 -0400

    First Commit Creating Repository

---
+++ js/site.js
@@ -0,0 +1,33 @@
+new WOW().init();
+
+var scroll = new SmoothScroll('[data-scroll]', {
+
+    speed: 1000,
+    offset: 0,
+    header: '.encabezado',
+
+});
+
+$(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 50) {
+            $(".ir-arriba").fadeIn();
+        } else {
+            $(".ir-arriba").fadeOut();
+        }
+    });
+
+});
+
+$(window).scroll(function () {
+
+    var nav = $('.encabezado');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
